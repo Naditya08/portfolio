7a9f05c2c867ef987aa1a17ccb03439bc08645e9
Nurukurthy L V V Aditya <113777713+Naditya08@users.noreply.github.com>
2025-11-25 15:55:33 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -18,7 +18,7 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = lokeshaditya08@gmail.com;
+  $contact->to =  $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
@@ -39,6 +39,7 @@
 
   echo $contact->send();
 ?>
+
 
 
 
